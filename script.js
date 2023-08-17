577e47e4d5f4796fbbb007e1b57a7f96617fea76
esthonisie <132005743+esthonisie@users.noreply.github.com>
2023-08-17 20:24:24 +0200

    Create function to show playing card images

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const btnHit = document.getElementById('btnHit');
 const btnStand = document.getElementById('btnStand');
 const btnDeal = document.getElementById('btnDeal');
-const cardsPlayer = document.querySelector('.cards.Player');
-const cardsDealer = document.querySelector('.cards.Dealer');
+const cardsPlayer = document.querySelector('.cardsPlayer');
+const cardsDealer = document.querySelector('.cardsDealer');
 const scorePlayer = document.querySelector('#scorePlayer');
 const scoreDealer = document.querySelector('#scoreDealer');
 const scoreDlrDeal = document.querySelector('#scoreDealer.hide');
 const infoPlayer = document.querySelectorAll('.info.Player');
 const allText = document.querySelectorAll(
-  '.info.Player, .cards.Dealer, .cards.Player, #scoreDealer, #scorePlayer'
+  '.info.Player, #scoreDealer, #scorePlayer'
 );
 
 const playRound = () => {
@@ -74,9 +74,10 @@ const deal = () => {
   }
   arrPlayer.push(result[0],result[2]);
   arrDealer.push(result[1],result[3]);
+  arrDlrDeal.push('Back');
   arrDlrDeal.push(result[3]);
-  cardsPlayer.textContent = `${result[0]} ${result[2]}`;
-  cardsDealer.textContent = `Rugzijde ${result[3]}`;
+  showCards(cardsPlayer, arrPlayer);
+  showCards(cardsDealer, arrDlrDeal);
   showScore();
   calcAceFixed(arrPlayer) === 21 || 
   calcAceFixed(arrDealer) === 21 ?
@@ -89,10 +90,8 @@ const hit = () => {
       Math.floor(Math.random()*arrShuffled.length), 1
     );
     arrPlayer.push(result[0]);
-    resetText(cardsPlayer);
-    for(let i = 0; i < arrPlayer.length; i++) {
-      cardsPlayer.textContent += `${arrPlayer[i]} `;
-    }
+    resetChild(cardsPlayer);
+    showCards(cardsPlayer, arrPlayer);
   }
   showScore();
   showInfoHit();
@@ -115,12 +114,14 @@ const calcAceFixed = arrPerson => {
   let points = 0;
   for(const item of arrPerson) {
     const result = item.charAt(0);
-    if(result === 'K' || result === 'Q' || result === 'J' || result === 'T') {
-        points += 10;
+    if(result === 'B') {
+      points += 0;
+    } else if(result === 'K' || result === 'Q' || result === 'J' || result === 'T') {
+      points += 10;
     } else if(result === 'A' && points < 11) {
-        points += 11;
+      points += 11;
     } else if(result === 'A' && points >= 11) {
-        points += 1;
+      points += 1;
     } else points += parseInt(result);
   } return points;
 };
@@ -130,7 +131,7 @@ const calcAceFluid = arrPerson => {
   for(const item of arrPerson) {
     const result = item.charAt(0);
     if(result === 'K' || result === 'Q' || result === 'J' || result === 'T') {
-        points += 10;
+      points += 10;
     } else if(result === 'A') {
       continue;
     } else points += parseInt(result);
@@ -138,7 +139,7 @@ const calcAceFluid = arrPerson => {
   for(const item of arrPerson) {
     const result = item.charAt(0);
     if(result === 'A' && points < 11) {
-        points += 11;
+      points += 11;
     } else if(result === 'A' && points >= 11) {
       points += 1;
     }
@@ -162,16 +163,24 @@ const calcDealer = () => {
   } else return calcAceLowScore(arrDealer, calcAceFluid(arrDealer));
 };
 
+const showCards = (cardsPerson, arrPerson) => {
+  for(let i = 0; i < arrPerson.length; i++) {
+    const imgCard = document.createElement('img');
+    imgCard.className = 'imgCard';
+    imgCard.src = `./images/${arrPerson[i]}.png`;
+    imgCard.style.zIndex = [i];
+    cardsPerson.appendChild(imgCard);
+  }
+};
+
 const showScore = () => {
   scorePlayer.textContent = `${calcPlayer()}`;
   if(switches.reveal === false) {
-      scoreDlrDeal.textContent = `${calcAceFixed(arrDlrDeal)} + ?`;
+    scoreDlrDeal.textContent = `${calcAceFixed(arrDlrDeal)} + ?`;
   } else if(switches.reveal === true) {
-      scoreDealer.textContent = `${calcDealer()}`;
-      resetText(cardsDealer);
-      for(let i = 0; i < arrDealer.length; i++) {
-        cardsDealer.textContent += `${arrDealer[i]} `;
-      }
+    scoreDealer.textContent = `${calcDealer()}`;
+    resetChild(cardsDealer);  
+    showCards(cardsDealer, arrDealer);
   } 
 };
 
@@ -186,73 +195,73 @@ const scoreTextDlr = () => {
 const showInfoBJack = () => {
   endGame();
   if(scoreTextPlr() === 21 && scoreTextDlr() !== 21) {
-      infoPlayer[0].textContent = "BLACKJACK";
-      infoPlayer[1].textContent = "Je hebt gewonnen!!";
-      infoPlayer[2].textContent = "Gefeliciteerd speler.";
-      infoPlayer[3].textContent = "Nog een ronde?"; 
+    infoPlayer[0].textContent = "BLACKJACK";
+    infoPlayer[1].textContent = "Je hebt gewonnen!!";
+    infoPlayer[2].textContent = "Gefeliciteerd speler.";
+    infoPlayer[3].textContent = "Nog een ronde?"; 
   } else if(scoreTextDlr() === 21 && scoreTextPlr() !== 21) {
-      infoPlayer[0].textContent = "Helaas.";
-      infoPlayer[1].textContent = "Je hebt verloren."; 
-      infoPlayer[2].textContent = "BLACKJACK";
-      infoPlayer[3].textContent = "Dealer heeft gewonnen.";    
+    infoPlayer[0].textContent = "Helaas.";
+    infoPlayer[1].textContent = "Je hebt verloren."; 
+    infoPlayer[2].textContent = "BLACKJACK";
+    infoPlayer[3].textContent = "Dealer heeft gewonnen.";    
   } else if(scoreTextPlr() === 21 && scoreTextDlr() === 21) {
-      infoPlayer[0].textContent = "BLACKJACK";
-      infoPlayer[1].textContent = "Helaas, ook de dealer heeft Blackjack."; 
-      infoPlayer[2].textContent = "BLACKJACK";
-      infoPlayer[3].textContent = "Het is gelijkspel geworden.";     
+    infoPlayer[0].textContent = "BLACKJACK";
+    infoPlayer[1].textContent = "Helaas, ook de dealer heeft Blackjack."; 
+    infoPlayer[2].textContent = "BLACKJACK";
+    infoPlayer[3].textContent = "Het is gelijkspel geworden.";     
   }
 }; 
 
 const showInfoHit = () => {
   if(scoreTextPlr() < 21) {
-      infoPlayer[0].textContent = "Nog een kaart? druk op 'Hit'.";
-      infoPlayer[1].textContent = "Geen kaart erbij? druk op 'Pas'.";
-      infoPlayer[2].textContent = "Dealer 16 punten of minder?";
-      infoPlayer[3].textContent = "Kaart erbij voor dealer."; 
+    infoPlayer[0].textContent = "Nog een kaart? druk op 'Hit'.";
+    infoPlayer[1].textContent = "Geen kaart erbij? druk op 'Pas'.";
+    infoPlayer[2].textContent = "Dealer 16 punten of minder?";
+    infoPlayer[3].textContent = "Kaart erbij voor dealer."; 
   } else if(scoreTextPlr() === 21 && scoreTextDlr() !== 21) {
-      endGame();
-      infoPlayer[0].textContent = "21 punten!";
-      infoPlayer[1].textContent = "Je hebt gewonnen!!";    
-      infoPlayer[2].textContent = "Gefeliciteerd speler.";
-      infoPlayer[3].textContent = "Nog een ronde?";   
+    endGame();
+    infoPlayer[0].textContent = "21 punten!";
+    infoPlayer[1].textContent = "Je hebt gewonnen!!";    
+    infoPlayer[2].textContent = "Gefeliciteerd speler.";
+    infoPlayer[3].textContent = "Nog een ronde?";   
   } else if(scoreTextPlr() === 22) {
-      endGame();
-      infoPlayer[0].textContent = "'BUST', 1 punt teveel.";
-      infoPlayer[1].textContent = "Je hebt verloren.";
-      infoPlayer[2].textContent = "Goed gespeeld, maar helaas.";
-      infoPlayer[3].textContent = "Volgende ronde beter?"; 
+    endGame();
+    infoPlayer[0].textContent = "'BUST', 1 punt teveel.";
+    infoPlayer[1].textContent = "Je hebt verloren.";
+    infoPlayer[2].textContent = "Goed gespeeld, maar helaas.";
+    infoPlayer[3].textContent = "Volgende ronde beter?"; 
   } else {
-      endGame();
-      infoPlayer[0].textContent = `'BUST', ${(scoreTextPlr() - 21)} punten teveel.`;
-      infoPlayer[1].textContent = "Je hebt verloren.";
-      infoPlayer[2].textContent = "Dat is spijtig speler.";
-      infoPlayer[3].textContent = "Revanche?"; 
+    endGame();
+    infoPlayer[0].textContent = `'BUST', ${(scoreTextPlr() - 21)} punten teveel.`;
+    infoPlayer[1].textContent = "Je hebt verloren.";
+    infoPlayer[2].textContent = "Dat is spijtig speler.";
+    infoPlayer[3].textContent = "Revanche?"; 
   }
 };
 
 const showInfoStand = () => {
   if(scoreTextDlr() <= 21) {
     if(scoreTextPlr() < scoreTextDlr()) {
-        infoPlayer[0].textContent = "Je hebt minder punten dan de dealer.";
-        infoPlayer[1].textContent = "Je hebt verloren.";    
-        infoPlayer[2].textContent = "Dat is spijtig speler.";
-        infoPlayer[3].textContent = "Revanche?"; 
+      infoPlayer[0].textContent = "Je hebt minder punten dan de dealer.";
+      infoPlayer[1].textContent = "Je hebt verloren.";    
+      infoPlayer[2].textContent = "Dat is spijtig speler.";
+      infoPlayer[3].textContent = "Revanche?"; 
     } else if(scoreTextPlr() === scoreTextDlr()) {
-        infoPlayer[0].textContent = "'STAND-OFF'.";
-        infoPlayer[1].textContent = "Geen winnaars.";    
-        infoPlayer[2].textContent = "Gelijkspel.";
-        infoPlayer[3].textContent = "Geen verliezers."; 
+      infoPlayer[0].textContent = "'STAND-OFF'.";
+      infoPlayer[1].textContent = "Geen winnaars.";    
+      infoPlayer[2].textContent = "Gelijkspel.";
+      infoPlayer[3].textContent = "Geen verliezers."; 
     } else if(scoreTextPlr() > scoreTextDlr()) {
-        infoPlayer[0].textContent = "Je hebt meer punten dan de dealer.";
-        infoPlayer[1].textContent = "Je hebt gewonnen!";    
-        infoPlayer[2].textContent = "Gefeliciteerd speler.";
-        infoPlayer[3].textContent = "Nog een ronde?";   
+      infoPlayer[0].textContent = "Je hebt meer punten dan de dealer.";
+      infoPlayer[1].textContent = "Je hebt gewonnen!";    
+      infoPlayer[2].textContent = "Gefeliciteerd speler.";
+      infoPlayer[3].textContent = "Nog een ronde?";   
     }
   } else if(scoreTextDlr() > 21) {
-      infoPlayer[0].textContent = "De dealer heeft teveel punten.";
-      infoPlayer[1].textContent = "Je hebt gewonnen!";    
-      infoPlayer[2].textContent = "'BUST'";
-      infoPlayer[3].textContent = "Gefeliciteerd speler.";   
+    infoPlayer[0].textContent = "De dealer heeft teveel punten.";
+    infoPlayer[1].textContent = "Je hebt gewonnen!";    
+    infoPlayer[2].textContent = "'BUST'";
+    infoPlayer[3].textContent = "Gefeliciteerd speler.";   
   }
 };
 
@@ -276,6 +285,12 @@ const resetArr = arr => {
   arr.length = 0;
 };
 
+const resetChild = cardsPerson => {
+  while(cardsPerson.firstChild) {
+    cardsPerson.removeChild(cardsPerson.firstChild);
+  }
+};
+
 const resetText = item => {
   item.textContent = '';
 };
@@ -291,4 +306,6 @@ const reset = () => {
   for(const text of allText) {
     resetText(text);
   }
+  resetChild(cardsPlayer);
+  resetChild(cardsDealer);
 };
